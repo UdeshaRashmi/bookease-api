4ec9059df9170d87699c37f4e95c00d6242947a4
Parana Manage Udesha Rashmi <rashmiparanamana1999@gmail.com>
2026-07-11 15:25:25 +0530

    chore(backend): enable CORS and exclude frontend from compilation

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,11 @@ import { ResponseInterceptor } from './common/interceptors/response.interceptor'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
